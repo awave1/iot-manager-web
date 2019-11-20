9e2fd7dad78f31dcb0b065a7d8955c8f2b1314cb
Artem Golovin <artgolovin96@gmail.com>
2019-11-20 00:56:37 -0700

    Remove unused imports/files

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../theme';

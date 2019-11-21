9ce9927b078feef0657724a094fd0864f76762ea
Artem Golovin <artgolovin96@gmail.com>
2019-11-20 23:48:15 -0700

    feat: generate random data every 5 seconds & remove unused graphs

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import { Grid, Paper } from '@material-ui/core';
-import { BarGraph, LineGraph, SeriesGraph } from '../graphs/';
 import Chart from 'react-apexcharts';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { useInterval } from '../../hooks';
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -61,169 +61,194 @@ const dummyData = {
   ],
 };
 
+const chartOptions = {
+  mixed: {
+    chart: {
+      id: 'basic-bar',
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        columnWidth: '50%',
+        endingShape: 'arrow',
+      },
+    },
+    stroke: {
+      width: [4, 0, 0],
+    },
+    xaxis: {
+      categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    markers: {
+      size: 6,
+      strokeWidth: 3,
+      fillOpacity: 0,
+      strokeOpacity: 0,
+      hover: {
+        size: 8,
+      },
+    },
+    yaxis: {
+      tickAmount: 5,
+      min: 0,
+      max: 100,
+    },
+  },
+  radial: {
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24,
+          },
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35,
+          },
+        },
+
+        dataLabels: {
+          showOn: 'always',
+          name: {
+            offsetY: -20,
+            show: true,
+            color: '#888',
+            fontSize: '13px',
+          },
+          value: {
+            formatter: function(val) {
+              return val;
+            },
+            color: '#111',
+            fontSize: '30px',
+            show: true,
+          },
+        },
+      },
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100],
+      },
+    },
+    stroke: {
+      lineCap: 'round',
+    },
+    labels: ['Percent'],
+  },
+  bar: {
+    chart: {
+      stacked: true,
+      stackType: '100%',
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      dropShadow: {
+        enabled: true,
+      },
+    },
+    stroke: {
+      width: 0,
+    },
+    xaxis: {
+      categories: ['Fav Color'],
+      labels: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    fill: {
+      opacity: 1,
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'vertical',
+        shadeIntensity: 0.35,
+        gradientToColors: undefined,
+        inverseColors: false,
+        opacityFrom: 0.85,
+        opacityTo: 0.85,
+        stops: [90, 0, 100],
+      },
+    },
+
+    legend: {
+      position: 'bottom',
+      horizontalAlign: 'right',
+    },
+  },
+};
+
+const getRandomVal = (max = 90, min = 20) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
 function Dashboard() {
   const classes = useStyles();
+  const [lineChartData, setLineChartData] = useState(dummyData.mixed);
+  const [radialChartData, setRadialChartData] = useState(dummyData.radial);
+  const [barChartData, setBarChartData] = useState(dummyData.bar);
 
-  const chartOptions = {
-    mixed: {
-      chart: {
-        id: 'basic-bar',
-        toolbar: {
-          show: false,
-        },
-      },
-      plotOptions: {
-        bar: {
-          columnWidth: '50%',
-          endingShape: 'arrow',
-        },
-      },
-      stroke: {
-        width: [4, 0, 0],
-      },
-      xaxis: {
-        categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      },
-      markers: {
-        size: 6,
-        strokeWidth: 3,
-        fillOpacity: 0,
-        strokeOpacity: 0,
-        hover: {
-          size: 8,
-        },
-      },
-      yaxis: {
-        tickAmount: 5,
-        min: 0,
-        max: 100,
-      },
-    },
-    radial: {
-      plotOptions: {
-        radialBar: {
-          startAngle: -135,
-          endAngle: 225,
-          hollow: {
-            margin: 0,
-            size: '70%',
-            background: '#fff',
-            image: undefined,
-            imageOffsetX: 0,
-            imageOffsetY: 0,
-            position: 'front',
-            dropShadow: {
-              enabled: true,
-              top: 3,
-              left: 0,
-              blur: 4,
-              opacity: 0.24,
-            },
-          },
-          track: {
-            background: '#fff',
-            strokeWidth: '67%',
-            margin: 0, // margin is in pixels
-            dropShadow: {
-              enabled: true,
-              top: -3,
-              left: 0,
-              blur: 4,
-              opacity: 0.35,
-            },
-          },
+  // Randomize the data every minute
+  useInterval(() => {
+    const updatedLineChartData = lineChartData.map(({ name, type, data }) => {
+      const updatedData = data.map(() => getRandomVal());
+      return { data: updatedData, type, name };
+    });
 
-          dataLabels: {
-            showOn: 'always',
-            name: {
-              offsetY: -20,
-              show: true,
-              color: '#888',
-              fontSize: '13px',
-            },
-            value: {
-              formatter: function(val) {
-                return val;
-              },
-              color: '#111',
-              fontSize: '30px',
-              show: true,
-            },
-          },
-        },
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          type: 'horizontal',
-          shadeIntensity: 0.5,
-          gradientToColors: ['#ABE5A1'],
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 100],
-        },
-      },
-      stroke: {
-        lineCap: 'round',
-      },
-      labels: ['Percent'],
-    },
-    bar: {
-      chart: {
-        stacked: true,
-        stackType: '100%',
-        toolbar: {
-          show: false,
-        },
-      },
-      plotOptions: {
-        bar: {
-          horizontal: true,
-        },
-      },
-      dataLabels: {
-        dropShadow: {
-          enabled: true,
-        },
-      },
-      stroke: {
-        width: 0,
-      },
-      xaxis: {
-        categories: ['Fav Color'],
-        labels: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
-      fill: {
-        opacity: 1,
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          type: 'vertical',
-          shadeIntensity: 0.35,
-          gradientToColors: undefined,
-          inverseColors: false,
-          opacityFrom: 0.85,
-          opacityTo: 0.85,
-          stops: [90, 0, 100],
-        },
-      },
+    const updatedBarChartData = barChartData.map(({ name, data }) => {
+      return {
+        data: [getRandomVal()],
+        name,
+      };
+    });
 
-      legend: {
-        position: 'bottom',
-        horizontalAlign: 'right',
-      },
-    },
-  };
+    setLineChartData(updatedLineChartData);
+    setRadialChartData([getRandomVal()]);
+    setBarChartData(updatedBarChartData);
+  }, 5000);
 
   return (
     <div className={classes.root}>
@@ -233,7 +258,7 @@ function Dashboard() {
             <Chart
               type="line"
               options={chartOptions.mixed}
-              series={dummyData.mixed}
+              series={lineChartData}
             />
           </Paper>
         </Grid>
@@ -243,7 +268,7 @@ function Dashboard() {
             <Chart
               type="radialBar"
               options={chartOptions.radial}
-              series={dummyData.radial}
+              series={radialChartData}
             />
           </Paper>
         </Grid>
@@ -254,7 +279,7 @@ function Dashboard() {
               type="bar"
               height={150}
               options={chartOptions.bar}
-              series={dummyData.bar}
+              series={barChartData}
             />
           </Paper>
         </Grid>

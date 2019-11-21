329add03d9604fd02450a9d6e2ca05b27933290b
Artem Golovin <artgolovin96@gmail.com>
2019-11-21 00:35:01 -0700

    fix: get rid of console errors that were caused by bar charts

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -29,16 +29,6 @@ const dummyData = {
       type: 'line',
       data: [30, 40, 25, 50, 49, 21, 70, 51],
     },
-    {
-      name: 'series-2',
-      type: 'column',
-      data: [23, 12, 54, 61, 32, 56, 81, 19],
-    },
-    {
-      name: 'series-3',
-      type: 'column',
-      data: [62, 12, 45, 55, 76, 41, 23, 43],
-    },
   ],
   radial: [76],
   bar: [

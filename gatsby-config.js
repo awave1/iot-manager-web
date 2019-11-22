3a3abca60638597b2622e8af2196297cdc7a6ea3
Artem Golovin <artgolovin96@gmail.com>
2019-11-22 14:42:42 -0700

    chore: remove unused data & images

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -44,7 +37,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-emotion',
